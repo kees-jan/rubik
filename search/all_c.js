01d1a55083d6d27190c7aa4fb2bc71e57d01f360
Documentation Builder <project@mandryn.org>
2016-01-09 16:24:29 +0000

    Automated documentation build for branch master, changeset 06b0ed300bc9c7a88b27d9110307a3336107a7e6.

--- search/all_c.js
+++ search/all_c.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['top',['top',['../classRubik_1_1Orientation.html#aac148ddc336d3f1c2de5fdd26cf66380',1,'Rubik::Orientation::top(int t)'],['../classRubik_1_1Orientation.html#acc93a9c5017bdce3ef9086bb1be89675',1,'Rubik::Orientation::top() const '],['../namespaceRubik.html#a2c2dde5401477b6af3f1b6bc22d83683',1,'Rubik::top()']]],
   ['top_5f',['top_',['../classRubik_1_1Orientation.html#a0f3e8c47d78c49cb2ad9ca53fff1ecd3',1,'Rubik::Orientation']]],
-  ['transform',['transform',['../namespaceanonymous__namespace_02side_8cc_03.html#a04e19bcd412b1f63b0aa50338c4099ae',1,'anonymous_namespace{side.cc}']]]
+  ['transform',['transform',['../namespaceanonymous__namespace_02side_8cc_03.html#a04e19bcd412b1f63b0aa50338c4099ae',1,'anonymous_namespace{side.cc}']]],
+  ['transformation',['transformation',['../classRubik_1_1Orientation.html#affee6b50206def9062db6f75d6013c50',1,'Rubik::Orientation']]]
 ];
