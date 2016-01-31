e7bf6a58785df2bedb14feed4796acb029c7340a
Documentation Builder <project@mandryn.org>
2016-01-31 12:43:22 +0000

    Automated documentation build for branch master, changeset b750837d0b2ad43154ea10ac445b39395be04831.

---
+++ hierarchy.js
@@ -0,0 +1,10 @@
+var hierarchy =
+[
+    [ "Rubik::CubeData", "classRubik_1_1CubeData.html", null ],
+    [ "Scroom::Utils::Detail::ErrorSignalHandler", "classScroom_1_1Utils_1_1Detail_1_1ErrorSignalHandler.html", null ],
+    [ "Rubik::FixedCube", "classRubik_1_1FixedCube.html", null ],
+    [ "Rubik::ISide", "classRubik_1_1ISide.html", [
+      [ "Rubik::Side", "classRubik_1_1Side.html", null ]
+    ] ],
+    [ "Rubik::Orientation", "classRubik_1_1Orientation.html", null ]
+];
--- search/all_2.js
+++ search/all_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['create',['create',['../classRubik_1_1FixedCube.html#a994de232df649945bde91fdcb8a8558f',1,'Rubik::FixedCube::create(CubeData const &amp;data)'],['../classRubik_1_1FixedCube.html#a8501f2243aa4203bc8e996aafb36fe7c',1,'Rubik::FixedCube::create(CubeData const &amp;data, boost::function&lt; ISide::Ptr(Side::Data)&gt; sideFactory)'],['../classRubik_1_1Side.html#accbe0c7bc5b82b06cae6ba536c4a4505',1,'Rubik::Side::create()']]],
+  ['create',['create',['../classRubik_1_1FixedCube.html#a994de232df649945bde91fdcb8a8558f',1,'Rubik::FixedCube::create(CubeData const &amp;data)'],['../classRubik_1_1FixedCube.html#a43039618b3763459ee214738b3c7a207',1,'Rubik::FixedCube::create(CubeData const &amp;data, boost::function&lt; ISide::Ptr(Side::Data const &amp;, Orientation const &amp;)&gt; sideFactory)'],['../classRubik_1_1Side.html#accbe0c7bc5b82b06cae6ba536c4a4505',1,'Rubik::Side::create()']]],
   ['cubedata',['CubeData',['../classRubik_1_1CubeData.html',1,'Rubik']]]
 ];
